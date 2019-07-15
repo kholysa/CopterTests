24da28861a2e4a7da8ee0a7256fbbc15c0d5d0e7
saif <kholysa@gmail.com>
2019-07-15 16:52:42 +0200

    Changes so it actually works

--- CopterMoveANDLocalisation/CopterMoveAndLocalisation.py
+++ CopterMoveANDLocalisation/CopterMoveAndLocalisation.py
@@ -2,7 +2,8 @@ from pyparrot.Bebop import Bebop
 import numpy
 
 
-def getRealLocation():
+def getRealLocation(bebop):
+    bebop.smart_sleep(5)
     # Will read the text file and provide the most up to date localisation information
     location = open('C:\\Users\\blab\\Desktop\\test.txt')
     lastLine = location.readlines()[-1]
@@ -10,11 +11,12 @@ def getRealLocation():
     x = float(lastLine.split(']')[0])
     y = float(lastLine.split(']')[1])
     location.close()
+    print(x,y)
     return (x,y)
 
 
-def orientCopter(flightDirection, expectedLocation):
-    realLocation = getRealLocation()
+def orientCopter(flightDirection, expectedLocation, bebop):
+    realLocation = getRealLocation(bebop)
     error = numpy.subtract(realLocation, expectedLocation)
     #fly forward
     if flightDirection == 1:
@@ -37,35 +39,39 @@ success = bebop.connect(2)
 flightDirection = int(input("Select your travel mode:\n\n1: Forward (Type 1)\n2: Backwards (Type 2)\n3: Left (Type 3)\n4: Right (Type 4)\n"))
 jumpSizes = float(input("Select your Jump size:\n\n5m Jumps (Type 5)\n2.5m Jumps (Type 2.5)"
                   "\n1m Jumps (Type 1)\n0.5m Jumps (Type 0.5)\n0.25m Jumps (Type 0.25)\n0.1m Jumps (Type 0.1)\n"))
-bebop.safe_takeoff(10)
-totalJump = 5
-numberOfJumps = int(totalJump/jumpSizes)
-for i in range(numberOfJumps):
-    print("Moving step number", i)
-    expectedLocation = (0,i * jumpSizes)
-    #fly forward
-    if flightDirection == 1:
-        bebop.move_relative(jumpSizes, 0, 0, 0)
-        # Orient Copter based on real position and expected position
-        orientCopter(flightDirection,expectedLocation)
-    # fly Backward
-    elif flightDirection == 2:
-        bebop.move_relative(-jumpSizes,0,0,0)
-        # Orient Copter based on real position and expected position
-        orientCopter(flightDirection, expectedLocation)
-    # fly Left
-    elif flightDirection == 3:
-        bebop.move_relative(0,-jumpSizes,0,0)
-        # Orient Copter based on real position and expected position
-        orientCopter(flightDirection, expectedLocation)
-    # fly Right
-    elif flightDirection == 4:
-        bebop.move_relative(0,jumpSizes,0,0)
-        # Orient Copter based on real position and expected position
-        orientCopter(flightDirection,expectedLocation)
+try:
+    bebop.safe_takeoff(10)
+    totalJump = 5
+    numberOfJumps = int(totalJump/jumpSizes)
+    firstPosition = getRealLocation(bebop)
+    for i in range(numberOfJumps):
+        print("Moving step number", i)
+        expectedLocation = numpy.add((0,(1+i) * jumpSizes),firstPosition)
+        #fly forward
+        if flightDirection == 1:
+            bebop.move_relative(jumpSizes, 0, 0, 0)
+            # Orient Copter based on real position and expected position
+            orientCopter(flightDirection,expectedLocation,bebop)
+        # fly Backward
+        elif flightDirection == 2:
+            bebop.move_relative(-jumpSizes,0,0,0)
+            # Orient Copter based on real position and expected position
+            orientCopter(flightDirection, expectedLocation,bebop)
+        # fly Left
+        elif flightDirection == 3:
+            bebop.move_relative(0,-jumpSizes,0,0)
+            # Orient Copter based on real position and expected position
+            orientCopter(flightDirection, expectedLocation,bebop)
+        # fly Right
+        elif flightDirection == 4:
+            bebop.move_relative(0,jumpSizes,0,0)
+            # Orient Copter based on real position and expected position
+            orientCopter(flightDirection,expectedLocation,bebop)
 
-print("Remaining battery: ",
-      bebop.sensors.battery)
-bebop.safe_land(10)
-bebop.disconnect()
-
+    print("Remaining battery: ",
+          bebop.sensors.battery)
+    bebop.safe_land(10)
+    bebop.disconnect()
+except:
+    bebop.safe_land(10)
+    bebop.disconnect()
